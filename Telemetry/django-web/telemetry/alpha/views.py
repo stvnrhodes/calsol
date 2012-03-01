e20a289b9f411b1ab3a31e4cb0e291936e60d1c9
kagelump@gmail.com <kagelump@users.noreply.github.com>
2012-03-01 07:48:26 +0000

    Working copy

--- Telemetry/django-web/telemetry/alpha/views.py
+++ Telemetry/django-web/telemetry/alpha/views.py
@@ -1,19 +1,25 @@
 from alpha.models import Car
 from alpha.models import DataPacket
 from django.shortcuts import render_to_response
+from django.views.decorators.csrf import csrf_protect
+from django.core.context_processors import csrf
 from django.http import HttpResponse
+from django.contrib import humanize
 import json
+import random
+import string
 
 class PostError(Exception):
   def __init__(self, msg=None):
     self.msg = msg
 
 def random_token(N):
-  return ''.join(random.choice(string.ascii_uppercase + string.digits)
+  return ''.join(random.choice(string.ascii_uppercase + string.digits + string.ascii_lowercase)
           for x in range(N))
 
 def cars(request):
   response = {}
+  response.update(csrf(request))
   response['cars'] = Car.objects.all()
   if request.method == 'POST':
     command = request.POST['command']
@@ -33,26 +39,40 @@ def car(request, car_id):
   response['data_packets'] = DataPacket.objects.filter(car=car).order_by('-time')
   return render_to_response('car.html', response)
   
+def car_json(request, car_id):
+  response = {}
+  car = Car.objects.get(pk=car_id)
+  data_packets = DataPacket.objects.filter(car=car).order_by('-time')
+  if not data_packets:
+    return HttpResponse(json.dumps({
+      'success': 'true',
+      'speed': 0,
+      'last_updated': 'Not connected.'
+    }))
+  first_packet = data_packets[0] or None
+  response['speed'] = first_packet.speed
+  response['time'] = 'Last updated %s' % humanize.naturaltime(first_packet.time)
+  response['success'] = 'true'
+  return HttpResponse(json.dumps(response))
+  
 # Post a data packet, either from a device or a test interface
 def post(request):
   response = {}
   try:
-    if request.method != 'POST':
-      raise PostError('Expect %s method, got %s as method.' % request.method)
-    if 'id' not in request.POST:
+    if 'id' not in request.REQUEST:
       raise PostError('Missing expected parameter: id')
-    if 'token' not in request.POST:
+    if 'token' not in request.REQUEST:
       raise PostError('Missing expected parameter: token')
-    car_id = request.POST['id']
-    token = request.POST['token']
+    car_id = request.REQUEST['id']
+    token = request.REQUEST['token']
     try:
       car = Car.objects.get(pk=car_id, token=token)
     except DoesNotExist:
       raise PostError('Invalid id or token')
     packet = DataPacket(car=car)
-    packet.speed = request.POST['speed'] or 0
-    packet.power = request.POST['power'] or 0
-    packet.battery_volt = request.POST['battery_volt'] or 0
+    packet.speed = request.REQUEST['speed'] or 0
+    packet.power = request.REQUEST['power'] or 0
+    packet.battery_volt = request.REQUEST['battery_volt'] or 0
     packet.save()
   except PostError(e):
     response['success'] = 'false'
