7ed7818e0093125e8811f03c2e244dfecf8f2f7f
kagelump@gmail.com <kagelump@users.noreply.github.com>
2012-04-21 02:15:26 +0000

    Segregates speed and tritium data

--- Telemetry/django-web/telemetry/alpha/models.py
+++ Telemetry/django-web/telemetry/alpha/models.py
@@ -6,7 +6,7 @@ class Car(models.Model):
   
 class DataPacket(models.Model):
   time = models.DateTimeField(auto_now_add=True)
-  speed = models.DecimalField(max_digits=5, decimal_places=2, default=0)
+  speed = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
   car = models.ForeignKey(Car)
   power = models.FloatField(blank=True, null=True)
   battery_volt = models.FloatField(blank=True, null=True)
