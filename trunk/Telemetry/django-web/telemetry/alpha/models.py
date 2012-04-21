dfcda9a79696a82c771e6161f6c5ddd1f36d5dc2
kagelump@gmail.com <kagelump@gmail.com@06f136d1-d091-2ae3-e27c-133c6a94ca8d>
2012-04-21 02:17:36 +0000

    Bug fix try

--- trunk/Telemetry/django-web/telemetry/alpha/models.py
+++ trunk/Telemetry/django-web/telemetry/alpha/models.py
@@ -12,5 +12,6 @@ class DataPacket(models.Model):
   battery_volt = models.FloatField(blank=True, null=True)
   lat = models.FloatField(blank=True, null=True)
   lng = models.FloatField(blank=True, null=True)
+  gps_speed = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
   def speed_as_mph(self):
     return float(self.speed) * 2.23693629
