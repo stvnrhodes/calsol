9644985d57afe25255a4de78e10c5e3037b8a19b
"RehdBlob" <dc37@users.noreply.github.com>
2012-05-19 01:39:13 +0000

    Continuing

--- DataloggerParser/src/dparser/vPerf.java
+++ DataloggerParser/src/dparser/vPerf.java
@@ -5,8 +5,6 @@ import java.util.ArrayList;
 /**
  * This is a "statistical performance measurement" as
  * documented by the Datalogger ASCII format specs.
- * It appears to have the same parameters as the statistical
- * voltage measurement, VoltageMessage.
  * It includes in its data ArrayList
  * a channel ID, the number of samples taken, min, avg, and max
  * values for the voltage measurements in the interval.
@@ -17,7 +15,11 @@ public class vPerf extends Message {
 	
 	public vPerf(String [] info, boolean ts) {
 		super(info,ts);
-		header.add("Statistical Voltage Performance");
+		header.add("Statistical Performance Measurement");
+		/* This is a little bit hard-coded, but then again,
+		 * this was the format given me.
+		 */
+		
 		data = new ArrayList<String>();
 		for (int i = 2; i < info.length; i++)
 			data.add(info[i]);
