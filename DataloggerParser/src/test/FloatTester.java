63182819b4394b00a0b65a23eb513e29e6a85480
"RehdBlob" <dc37@users.noreply.github.com>
2012-05-23 21:06:21 +0000

    Integer overflow issues. Working on fixing.

--- DataloggerParser/src/test/FloatTester.java
+++ DataloggerParser/src/test/FloatTester.java
@@ -2,6 +2,7 @@ package test;
 
 /**
  * Float decoding tester.
+ * There appears to be integer wraparound.
  * @author Derek Chou
  * @since 2012.05.21
  */
@@ -11,7 +12,9 @@ public class FloatTester {
 	 * @param args : Main class String [] args thing.
 	 */
 	public static void main(String[] args) {
-		String test = "C0000000";
+		String test = "AF477540";
+		Long l = Long.parseLong(test, 16);
+		System.out.println(Long.toBinaryString(l));
 		float asFloat = Float.intBitsToFloat((int) Long.parseLong(test, 16));
 		System.out.println(asFloat);
 	}
