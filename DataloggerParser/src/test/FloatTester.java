713765ebb901551f044d6d96726f19b64dbaaa4d
"RehdBlob" <dc37@users.noreply.github.com>
2012-05-25 05:20:02 +0000

    Input is generally good now; working on output. Also generated Javadocs.

--- DataloggerParser/src/test/FloatTester.java
+++ DataloggerParser/src/test/FloatTester.java
@@ -13,12 +13,11 @@ public class FloatTester {
 	 * @param args : Main class String [] args thing.
 	 */
 	public static void main(String[] args) {
-		String test = "407547AF";
+		String test = "43010560";
 		Long l = Long.parseLong(test, 16);
-		System.out.println(Long.toBinaryString(l));
 		float asFloat = Float.intBitsToFloat((int)Long.parseLong(test, 16));
 		double asDouble = Double.longBitsToDouble(l);
-		System.out.println((double)asFloat);
+		System.out.println(asFloat);
 		System.out.println(asDouble);
 		System.out.println(Integer.parseInt("EC", 16));
 	}
